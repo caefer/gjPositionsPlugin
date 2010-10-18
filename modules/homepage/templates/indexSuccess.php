bd84d785e3819a6fa1add2d69812bf88ce74477a
caefer <caefer@ical.ly>
2010-10-18 21:39:44 +0200

    * removed homepage module as this functionality will be implemented in the project

--- modules/homepage/templates/indexSuccess.php
+++
@@ -1,9 +0,0 @@
-<?php decorate_with($page['layoutName']); ?>
-<h1><?php echo $page['title']; ?></h1>
-<?php foreach($page['DesignElements'] as $element): ?>
-<?php   if(is_string($partials[$element['name']])): ?>
-<?php     include_partial((string)$partials[$element['name']], array('contents' => $element['Contents'])); ?>
-<?php   else: ?>
-<?php     include_component($partials[$element['name']][0], $partials[$element['name']][1], array('contents' => $element['Contents'])); ?>
-<?php   endif; ?>
-<?php endforeach; ?>
