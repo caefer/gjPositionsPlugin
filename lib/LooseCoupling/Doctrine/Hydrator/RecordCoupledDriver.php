46aab22f3217bc423dd60ac137922f640eea8a4c
caefer <caefer@ical.ly>
2010-11-10 09:06:35 +0100

    * removed LooseCoupling as a submodule to be relaced by a hard copy as it is tedious to have the tests folder checked out producing errors as doctrine is not complete (tests are missing)

---
+++ lib/LooseCoupling/Doctrine/Hydrator/RecordCoupledDriver.php
@@ -0,0 +1,68 @@
+<?php
+class Doctrine_Hydrator_RecordCoupledDriver extends Doctrine_Hydrator_RecordDriver
+{
+  protected $_looseObjects = array();
+  protected $_applicableAliases = null;
+
+  public function hydrateResultSet($stmt)
+  {
+    $this->rootAlias = key($this->_queryComponents);
+    $results = parent::hydrateResultSet($stmt);
+    $this->_collectObjects();
+    return $results;
+  }
+
+  protected function _gatherRowData(&$data, &$cache, &$id, &$nonemptyComponents)
+  {
+    $rowData = parent::_gatherRowData($data, $cache, $id, $nonemptyComponents);
+    foreach($this->getApplicableAliases($rowData) as $alias)
+    {
+      if(!empty($rowData[$alias]['obj_type']) && !empty($rowData[$alias]['obj_pk']))
+      {
+        $this->_looseObjects[$rowData[$alias]['obj_type']][$rowData[$alias]['obj_pk']] = $rowData[$alias]['obj_pk'];
+        $rowData[$alias]['Object'] = &$this->_looseObjects[$rowData[$alias]['obj_type']][$rowData[$alias]['obj_pk']];
+      }
+    }
+    return $rowData;
+  }
+
+  protected function getApplicableAliases($rowData = array())
+  {
+    if(is_null($this->_applicableAliases))
+    {
+      $this->_applicableAliases = array();
+      foreach(array_keys($rowData) as $alias)
+      {
+        if(array_key_exists('obj_type', $rowData[$alias]) && array_key_exists('obj_pk', $rowData[$alias]))
+        {
+          $this->_applicableAliases[] = $alias;
+        }
+      }
+    }
+
+    return $this->_applicableAliases;
+  }
+
+  protected function _collectObjects()
+  {
+    foreach($this->_looseObjects as $type => $ids)
+    {
+      $table      = Doctrine_Core::getTable($type);
+      $identifier = $table->getIdentifierColumnNames();
+
+      if(1 != count($identifier))
+      {
+        throw new Doctrine_Hydrator_Exception("Couldn't hydrate. LooseCoupling does not support multi column primary keys!.");
+      }
+
+      $objects = $table->createQuery('o')
+                       ->whereIn('o.'.$identifier[0], $ids)
+                       ->execute(array(), Doctrine_Core::HYDRATE_RECORD);
+
+      foreach($objects as $object)
+      {
+        $this->_looseObjects[$type][$object->id] = $object;
+      }
+    }
+  }
+}
---
+++ lib/LooseCoupling/Doctrine/Template/Listener/LooselyCoupled.php
@@ -0,0 +1,66 @@
+<?php
+
+class LooselyCoupledListener extends Doctrine_Record_Listener
+{
+  protected $_options = array();
+
+  public function __construct($options)
+  {
+    $this->_options = $options;
+  }
+
+  public function postDelete(Doctrine_Event $event)
+  {
+    $subject = $event->getInvoker();
+    $type = get_class($subject);
+    $pk = current($subject->identifier());
+
+    foreach($this->_options as $modelClass)
+    {
+      Doctrine_Query::create()
+        ->delete($modelClass.' o')
+        ->addWhere('o.obj_type = ?', $type)
+        ->addWhere('o.obj_pk = ?', $pk)
+        ->execute();
+    }
+  }
+
+  public function preDqlSelect(Doctrine_Event $event)
+  {
+    $query = $event->getQuery();
+
+    $relations = array_keys($this->_options);
+
+    $components = $this->_getDqlCallbackComponents($query);
+    foreach ($components as $alias => $component)
+    {
+      if (isset($component['relation']))
+      {
+        $query->addWhere($alias.'.obj_type = ?', get_class($event->getInvoker()));
+      }
+    }
+  }
+
+  protected function _getDqlCallbackComponents($query)
+  {
+    $params = $query->getParams();
+    $componentsBefore = array();
+    if ($query->isSubquery())
+    {
+      $componentsBefore = $query->getQueryComponents();
+    }
+
+    $copy = $query->copy();
+    $copy->getSqlQuery($params);
+    $componentsAfter = $copy->getQueryComponents();
+
+    if ($componentsBefore !== $componentsAfter)
+    {
+      return array_diff($componentsAfter, $componentsBefore);
+    }
+    else
+    {
+      return $componentsAfter;
+    }
+  }
+}
