9a77713e36c9b0749faaa2f720f50bd79a67bd3b
caefer <caefer@ical.ly>
2010-11-10 15:48:16 +0100

    * now design elements can be dropped onto the canvas

--- web/js/positions.js
+++ web/js/positions.js
@@ -12,24 +12,32 @@ gjCompositionCanvas = function(form)
     {
       $(designelement).find('input, select, textarea').each(function(j, field)
       {
+        // update name
         field.name = field.name
           .replace(/\[design_elements\]\[(?:\d|x)\]/, '[design_elements]['+i+']')
           .replace(/\[contents\]\[(?:\d|x)\]/, '[contents]['+j+']');
+        // update id
         field.id = field.id
           .replace(/design_elements_(?:\d|x)_/, 'design_elements_'+i+'_')
           .replace(/contents_(?:\d|x)_/, 'contents_'+j+'_');
+        // update position
+        if(-1 != field.name.search(/\[design_elements\]\[(?:\d|x)\]\[position\]/))
+        {
+          field.value = i;
+        }
       });
     });
   };
 
-  this.form.find('.content > .positions_container')
-    .sortable(
-    {
+  this.form.find('.content > .positions_container').sortable({
       items:'li:not(.placeholder,.content_element_item)',
       axis:'y',
-      sort:function(){$(this).removeClass('ui-state-default');},
-      update:function(){$(this).find('input[name$=\[position\]]').not('input[name*=\[contents\]]').each(function(i,element){$(element).val(i);});}
     });
+
+  $('#design_element_source_list li').draggable({
+    connectToSortable:$('.content > .positions_container'),
+    helper:'clone'
+  })
 
   // bind events
   this.form.submit(this.updatePositions);
