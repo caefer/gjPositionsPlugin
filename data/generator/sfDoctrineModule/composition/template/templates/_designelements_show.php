4d615688354a43c5677958118932d60337f6b6a1
caefer <caefer@ical.ly>
2010-11-10 14:06:31 +0100

    * removed collapsed state

--- data/generator/sfDoctrineModule/composition/template/templates/_designelements_show.php
+++ data/generator/sfDoctrineModule/composition/template/templates/_designelements_show.php
@@ -6,6 +6,6 @@
           [?php is_string($config['include']) ? include_partial($config['include'], array('params' => $params)) : include_component($config['include'][0], $config['include'][1], array('params' => $params)); ?]
         </div>
       </div>
-      <div class="design-element-form" id="form_[?php echo $name; ?]" style="display:[?php echo $collapsed ? 'none' : 'block'; ?]">
+      <div class="design-element-form" id="form_[?php echo $name; ?]">
         [?php if(isset($form)) echo $form; ?]
       </div>
