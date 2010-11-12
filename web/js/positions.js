e8b49196c2d7af387f92f765eee55c1408b14110
caefer <caefer@ical.ly>
2010-11-12 11:23:15 +0100

    * content elements are now draggable and updating positions now works correctly

--- web/js/positions.js
+++ web/js/positions.js
@@ -8,30 +8,43 @@ gjCompositionCanvas = function(form)
   this.updatePositions = function(event)
   {
     form = $(event.currentTarget);
-    form.find('.content > ol > li').each(function(i, designelement)
+    form.find('.content > .positions_container > li').each(function(i, designelement)
     {
       $(designelement).find('input, select, textarea').each(function(j, field)
       {
         // update name
-        field.name = field.name
-          .replace(/\[design_elements\]\[(?:\d|x)\]/, '[design_elements]['+i+']')
-          .replace(/\[contents\]\[(?:\d|x)\]/, '[contents]['+j+']');
+        field.name = field.name.replace(/\[design_elements\]\[(?:\d|x)\]/, '[design_elements]['+i+']')
         // update id
-        field.id = field.id
-          .replace(/design_elements_(?:\d|x)_/, 'design_elements_'+i+'_')
-          .replace(/contents_(?:\d|x)_/, 'contents_'+j+'_');
+        field.id = field.id.replace(/design_elements_(?:\d|x)_/, 'design_elements_'+i+'_')
         // update position
         if(-1 != field.name.search(/\[design_elements\]\[(?:\d|x)\]\[position\]/))
         {
           field.value = i;
         }
       });
+      $(designelement).find('.positions_container > li').each(function(j, contentelement)
+      {
+        $(contentelement).find('input, select, textarea').each(function(k, field)
+        {
+          // update name
+          field.name = field.name.replace(/\[contents\]\[(?:\d|x)\]/, '[contents]['+j+']');
+          // update id
+          field.id = field.id.replace(/contents_(?:\d|x)_/, 'contents_'+j+'_');
+          // update position
+          if(-1 != field.name.search(/\[contents\]\[(?:\d|x)\]\[position\]/))
+          {
+            field.value = j;
+          }
+          console.log(field.name+' => '+field.value);
+        });
+      });
     });
+    form.unbind();
+    return false;
   };
 
   this.form.find('.content > .positions_container').sortable({
-//      items:'li:not(.placeholder,.content_element_item)',
-      //axis:'y',
+      axis:'y',
       placeholder: 'ui-state-highlight',
       forcePlaceholderSize: true,
       receive: function(event, ui){$(event.target).find('li:.ui-draggable').removeClass('ui-draggable').children('div').toggle();},
@@ -40,8 +53,23 @@ gjCompositionCanvas = function(form)
       beforeStop: function(event, ui){if(1 == removeMe) ui.item.remove()}
     });
 
-  $('#design_element_source_list li').draggable({
+  this.form.find('.content .design-element-form .positions_container').sortable({
+      axis:'y',
+      placeholder: 'ui-state-highlight',
+      forcePlaceholderSize: true,
+      receive: function(event, ui){$(event.target).find('li:.ui-draggable').removeClass('ui-draggable').children('div').toggle();},
+      over: function(event, ui){removeMe = 0},
+      out: function(event, ui){removeMe = 1},
+      beforeStop: function(event, ui){if(1 == removeMe) ui.item.remove()}
+    });
+
+  $('#design_element_source_list > li').draggable({
     connectToSortable:$('.content > .positions_container'),
+    helper:'clone'
+  })
+
+  $('#content_element_source_list .contents > li').draggable({
+    connectToSortable:$('.content .design-element-form .positions_container'),
     helper:'clone'
   })
 
