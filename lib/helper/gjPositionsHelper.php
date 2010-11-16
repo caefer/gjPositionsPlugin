5e92cc46edc0881f9cbd9f2d53c1ad3f6c03d962
caefer <caefer@ical.ly>
2010-11-16 14:29:54 +0100

    * include_design_element() helper now passes the subject as well as the params

--- lib/helper/gjPositionsHelper.php
+++ lib/helper/gjPositionsHelper.php
@@ -7,12 +7,17 @@ function include_design_element($designElement)
 
 function get_design_element($designElement)
 {
-   $configs = sfConfig::get('app_gjPositionsPlugin_design_elements', array());
+  $configs = sfConfig::get('app_gjPositionsPlugin_design_elements', array());
 
-   if(is_string($include = $configs[$designElement['name']]['include']))
-   {
-     return get_partial($include, array('params' => $designElement['params']));
-   }
+  $options = array(
+    'params'  => $designElement['params'],
+    'subject' => $designElement->getObject()
+  );
 
-   return get_component($include[0], $include[1], array('params' => $designElement['params']));
+  if(is_string($include = $configs[$designElement['name']]['include']))
+  {
+    return get_partial($include, $options);
+  }
+
+  return get_component($include[0], $include[1], $options);
 }
