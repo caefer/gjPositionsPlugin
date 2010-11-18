82fb8646549ff4794cba6e598d18890b1ce056b8
caefer <caefer@ical.ly>
2010-11-18 19:20:16 +0100

    * added table proxy revrieval method to enable in routing. this will reduce the number of queries in the admin module

--- lib/template/gjCompositionCanvas.class.php
+++ lib/template/gjCompositionCanvas.class.php
@@ -6,4 +6,13 @@ class gjCompositionCanvas extends LooselyCoupled
   {
     parent::__construct(array('DesignElements' => 'gjDesignElement'));
   }
+
+  public function getObjectTableProxy(array $params)
+  {
+    return $this->getInvoker()->getTable()->createQuery('c')
+      ->where('c.id = ?', $params['id'])
+      ->leftJoin('c.DesignElements de')
+      ->leftJoin('de.Contents ce')
+      ->execute(array(), gjPositionsPluginConfiguration::HYDRATE_RECORD_COUPLED);
+  }
 }
