35217caa37c8b7880322482a080efafcde6aa477
caefer <caefer@ical.ly>
2010-11-28 21:23:39 +0100

    * reset to original skeleton apart from components.class.php

--- data/generator/sfDoctrineModule/composition/skeleton/lib/configuration.php
+++ data/generator/sfDoctrineModule/composition/skeleton/lib/configuration.php
@@ -3,9 +3,9 @@
 /**
  * ##MODULE_NAME## module configuration.
  *
- * @package     gjPositionsPlugin
- * @subpackage  <?php echo $this->getModuleName()."\n" ?>
- * @author      Christian Schaefer <caefer@ical.ly>
+ * @package    ##PROJECT_NAME##
+ * @subpackage ##MODULE_NAME##
+ * @author     ##AUTHOR_NAME##
  * @version    SVN: $Id: configuration.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
 class ##MODULE_NAME##GeneratorConfiguration extends Base##UC_MODULE_NAME##GeneratorConfiguration
--- data/generator/sfDoctrineModule/composition/skeleton/lib/helper.php
+++ data/generator/sfDoctrineModule/composition/skeleton/lib/helper.php
@@ -3,9 +3,9 @@
 /**
  * ##MODULE_NAME## module helper.
  *
- * @package     gjPositionsPlugin
- * @subpackage  <?php echo $this->getModuleName()."\n" ?>
- * @author      Christian Schaefer <caefer@ical.ly>
+ * @package    ##PROJECT_NAME##
+ * @subpackage ##MODULE_NAME##
+ * @author     ##AUTHOR_NAME##
  * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
 class ##MODULE_NAME##GeneratorHelper extends Base##UC_MODULE_NAME##GeneratorHelper
