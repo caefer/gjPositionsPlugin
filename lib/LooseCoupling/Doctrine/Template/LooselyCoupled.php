b2e5618c2ef3984d24bd546d0aaa7f23afeed7c8
caefer <caefer@ical.ly>
2010-12-02 08:38:00 +0100

    * reinserted filter for collection of loosely coupledc relations

--- lib/LooseCoupling/Doctrine/Template/LooselyCoupled.php
+++ lib/LooseCoupling/Doctrine/Template/LooselyCoupled.php
@@ -16,6 +16,37 @@ class LooselyCoupled extends Doctrine_Template
     foreach($this->_options as $alias => $modelName)
     {
       $this->hasMany($modelName.' as '.$alias, array('local' => 'id', 'foreign' => 'obj_pk'));
+      $this->getInvoker()->hasAccessor($alias, 'fetchLooselyCoupledRelation');
     }
+  }
+
+  public function fetchLooselyCoupledRelation($load, $field)
+  {
+    $record = $this->getInvoker();
+    $record->clearAccessor($field);
+
+    if($record->hasReference($field))
+    {
+      return $record->reference($field);
+    }
+
+    $record->loadReference($field);
+    $collection = $this->filterWrongRelations($record, $field);
+    $record->setRelated($field, $collection);
+
+    return $collection;
+  }
+
+  protected function filterWrongRelations($record, $field)
+  {
+    $collection = $record->reference($field);
+    foreach($collection as $key => $related)
+    {
+      if($related['obj_type'] != $record->getTable()->getComponentName())
+      {
+        $collection->remove($key);
+      }
+    }
+    return $collection;
   }
 }
