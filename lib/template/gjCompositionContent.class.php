491ec0b0cd026a5ef4e1c5f127015397969f3ebe
caefer <caefer@ical.ly>
2011-01-27 16:15:37 +0100

    * added field type override feature (experimental)

--- lib/template/gjCompositionContent.class.php
+++ lib/template/gjCompositionContent.class.php
@@ -16,11 +16,21 @@ class gjCompositionContent extends Doctrine_Template
     $fieldNames = isset($this->_options['override']) ? $this->_options['override'] : array();
     foreach($fieldNames as $fieldName)
     {
+      if(is_array($fieldName))
+      {
+        $type = current($fieldName);
+        $fieldName = key($fieldName);
+      }
+
       $fieldDefinition = $this->getInvoker()->getTable()->getColumnDefinition($fieldName);
+
       switch($fieldDefinition['type'])
       {
+        case 'decimal':
+        case 'float':
+        case 'integer':
         case 'string':
-          $overrideFields[$fieldName] = array('type' => 'text', 'default' => '');
+          $overrideFields[$fieldName] = array('type' => (isset($type) ? $type : 'text'), 'default' => '');
           break;
         default:
           throw new sfException('Overriding of columns of type "'.$fieldDefinition['type'].'" are not implemented!');
